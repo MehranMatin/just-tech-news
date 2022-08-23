a5fb6ef082e461984d08aed949a7ee846e5d6d36
Mehran Matin <81208724+MehranMatin@users.noreply.github.com>
2022-08-22 23:12:34 -0400

    post routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -20,7 +20,13 @@ router.get('/:id', (req, res) => {
         attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
-        }
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            }
+        ]
     })
         .then(dbUserData => {
             if (!dbUserData) {
@@ -66,6 +72,7 @@ router.post('/login', (req, res) => {
 
         // Verify user
         const validPassword = dbUserData.checkPassword(req.body.password);
+        
         if (!validPassword) {
             res.status(400).json({ message: 'Incorrect Password!' });
             return;
